32ce7e4e20c9634860d47777cc8c42e099db57a6
Jonathan <jonopan1196@gmail.com>
2020-04-05 01:46:35 +1100

    New Login/SignUp Pages (according to James Wireframes) *sortof.......

--- html/js/myScript.js
+++ html/js/myScript.js
@@ -1,11 +1,12 @@
 const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
+const logInButton = document.getElementById('LogIn');
 const container = document.getElementById('container');
 
+/*Sliding between panel*/
+logInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});

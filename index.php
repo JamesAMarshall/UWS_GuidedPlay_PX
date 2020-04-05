5c7769f5610a50eb767f595282dedf796ea4a8cf
James <jmarshall.bb8@gmail.com>
2020-04-05 18:08:24 +1000

    Html Page Skeleton Built
    Added in all the pages needed so far and linked in the necessary navigation on the pages. Plus added log in and and sign up fields to the home page

--- index.php
+++ index.php
@@ -8,11 +8,62 @@
 </head>
 <body>
         <h1>Home Page</h1>
-        Welcome to our homepage: <?php echo $_SERVER['HTTP_HOST']; ?>
+        
+        <nav>
+                <a href="index.php">Home</a>
+                <a href="html/home_student.html">Student Home</a>
+                <a href="html/home_teacher.html">Teacher Home</a>
+                <a href="html/home_researcher.html">Researcher Home</a>
+                <a href="html/home_admin.html">Admin Home</a>
+        </nav>
+
+        <!-- Welcome to our homepage: <?php echo $_SERVER['HTTP_HOST']; ?> -->
+        
         <br>
+
         <p>
                 <?php include "php/main.php"; ?>
         </p>
+
+        <h2>Log In</h2>
+
+        <label for="">username</label>
+        <input type="text">
+        
+        <label for="">password</label>
+        <input type="password">
+
+        <button>Log In</button>
+
+        <br>
+
+        <label for="">account type</label>
+        <input type="text">
+
+        <br>
+
+        <h2>Sign Up</h2>
+
+        <label for="">username</label>
+        <input type="text">
+        <br>
+        <label for="">password</label>
+        <input type="password">
+<br>
+        <label for="">confirm password</label>
+        <input type="password">
+<br>
+        <label for="">date of birth</label>
+        <input type="text">
+<br>
+        <label for="">gender</label>
+        <input type="text">
+
+
+
+        <button>Sign Up</button>
+
+        
         
 </body>
 </html>

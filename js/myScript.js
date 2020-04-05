3884fca40c8e5c4d7538300dfc1124bc94f38b67
Jonathan <jonopan1196@gmail.com>
2020-04-06 02:33:37 +1000

    Fixed up Sign Up/LogIn Page + Homepage for each users

---
+++ js/myScript.js
@@ -0,0 +1,17 @@
+const signUpButton = document.getElementById('signUp');
+const logInButton = document.getElementById('LogIn');
+const container = document.getElementById('container');
+
+/*Sliding between panel*/
+logInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+/* return function */
+function goBack() {
+    window.history.back()
+}
+

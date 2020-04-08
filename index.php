ac34ea32e0a6e43d77220e20b98c609992a6fd08
James <jmarshall.bb8@gmail.com>
2020-04-08 14:13:16 +1000

    Merging All members work to this branch

--- index.php
+++ index.php
@@ -1,101 +1,139 @@
 <!DOCTYPE html>
-<html lang="en">
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
 <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Guided Play</title>
-        <script src="js/formValidation.js" type="text/javascript"></script>
-        <link rel="stylesheet" type="text/css" href="css/theme.css">
-        
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Home Page</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
+  <link href="vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="vendor/aos/aos.css" rel="stylesheet">
+  
+
+  <!-- Template Main CSS File -->
+  <link href="css/style.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/theme.css">
+  <script src="js/formValidation.js"></script>
 </head>
-<body>
-        <h1>Home Page</h1>
+    <body>
+		<header id="header" class="fixed-top">
+		<div class="container d-flex">
+        <div class="logo mr-auto">
+        <h1 class="text-light"><a href="index.html">Welcome to Guided Play</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+        </div>
         
-        <nav>
-                <a href="index.php">Home</a>
-                <a href="html/home_student.html">Student Home</a>
-                <a href="html/home_teacher.html">Teacher Home</a>
-                <a href="html/home_researcher.html">Researcher Home</a>
-                <a href="html/home_admin.html">Admin Home</a>
+        <form action ="php/requests/login.php" method="post" id="post-form" onsubmit="return validateLogInForm(this);">
+        <nav class="nav-menu d-none d-lg-block">
+        <ul>
+			<li class="active"><a href="index.php">Home</a></li>		
+			<li> <a href="html/home_student.html">Student Home</a></li>	
+			<li> <a href="html/home_teacher.html">Teacher Home</a></li>	
+			<li> <a href="html/home_researcher.html">Researcher Home</a></li>	
+			<li> <a href="html/home_admin.html">Admin Home</a></li>	
+			<li class="get-started"><a href="index.php">Sign Out</a></li>
+		</ul>
         </nav>
+        <br/>
+		<br/>
+		<br/>
+		<h2>Log In</h2>
+		<p>
+		<span class="required">*</span>Required field
+		</p>
+            <div>
+                <label for="username">username</label>
+                <input type="text" id="uname" name="uname"/>
+				<span class="required">*</span>
+                <span class="error-message" id="usernameError">username required</span>
+                <br/>
+                
+                <label for="password">password</label>
+                <input type="password" id="pass" name="pass"/>
+				<span class="required">*</span>
+                <span class="error-message" id="passwordError">password required</span>
+                <br/>
 
-        Welcome to our homepage: <?php echo $_SERVER['HTTP_HOST']; ?>
-        
-        <br>
-
-        <!-- <p>
-                <?php include "php/requests/login.php"; ?>
-        </p> -->
-        <form action ="php/requests/login.php" method="post" id="post-form" >
-        <h2>Log In</h2>
-                <div>
-                        <label for="username">username</label>
-                        <input type="text" id="uname" name="uname"/>
-                        <span class="error-message" id="usernameError">username required</span>
-                        <br/>
-                        
-                        <label for="password">password</label>
-                        <input type="text" id="pass" name="pass"/>
-                        <span class="error-message" id="passwordError">password required</span>
-                        <br/>
-                        <br/><br/>
-                        <input type="submit" value="Log In" name="submit">
-                </div>
-        </form>
-
-        <form action ="#" method="post" id="post-form" onsubmit="return validateForm(this);">
-      
-        <h2>Sign Up</h2>
+            </div>
             <div>
 
+                
+                <br/>
+                <input type="submit" value="Log In" name="submit">
+				<br/>
+				<br/>
+            </div>
+        </form>
+        <form action ="php/requests/login.php" method="post" id="post-form" onsubmit="return validateForm(this);">
+        <h2>Sign Up</h2>
+		<br/>
+            <div>
+                <label for="username">username</label>
+                <input type="text" id="sname" name="sname"/>
+				<span class="required">*</span>
+                <span class="error-message" id="username2Error">username required</span>
+                  
+                <br/>
+				
+                <label for="password">password</label>
+                <input type="password" id="pwd1" />
+				<span class="required">*</span>
+				<br>			
+                <label for="password">confirm password</label>
+                <input type="password" id="pwd2"/>
+				<span class="required">*</span>
+				<span class="error-message" id="pwd2Error">Password do not matched</span>
+
+                <br/>
+                
                 <label for="accountType">account type</label>
                 <select id="accountType" name="accountType" height="50px">
+                        <option value="default"selected disabled></option>
                         <option value="student">Student</option>
                         <option value="teacher">Teachers</option>
                         <option value="community">Community</option>
                         <option value="admin">Administrator</option>
                         <option value="reseacher">Researcher</option>
                 </select>
-                        
-
-                <label for="username">username</label>
-                <input type="text" id="sname" name="sname"/>
-                <span class="error-message" id="username2Error">username required</span>
-                
-                <br/>
-                <label for="password">password</label>
-                <input type="text" id="pwd" name="pwd1"/>
-                <br/>
-                <label for="password">confirm password</label>
-                <input type="text" id="pwd" name="pwd2"/>
-                <br/>
-                
+                                        
                 <label for="dateofbirth">date of birth</label>
-                <input type="date" id="dob" name="dob"/>
-                <br/>
-                
-                
-                <label for="dateofbirth">date of birth</label>
-		<input type="text" id="dob2" name="dob2" placeholder="DD/MM/YYYY"/>
-		<span class="error-message" id="dobError">Date of Birth required</span>
-		<span class="error-message" id="dobFormatError">Check date format</span>
-		<span class="error-message" id="dobInputError">Check date input</span>
+				<input type="text" id="dob2" name="dob2" placeholder="DD/MM/YYYY"/>
+				<span class="required">*</span>
+				<span class="error-message" id="dobError">Date of Birth required</span>
+				<span class="error-message" id="dobFormatError">Check date format</span>
+				<span class="error-message" id="dobInputError">Check date input</span>
                 <br/>
                 
                 <label for="gender">gender</label>
-                <input type="radio" name="male" id="male">
+                <input type="radio" name="gender" id="male">
                 <label for="male">male</label>
-                <input type="radio" name="female" id="female">
+                <input type="radio" name="gender" id="female">
                 <label for="female">female</label>
-                
-                <br/><br/>
+				<span class="required">*</span>
+				<span class="error-message" id="genderError"> Please Select a gender</span>
+				<br>
+				
                 <input type="submit" value="Sign Up">
             </div>
         </form>    
 
       </body>
-
-        
-        
-</body>
+    
 </html>
--- js/formValidation.js
+++ js/formValidation.js
@@ -1,3 +1,55 @@
+ 
+ 
+ 
+ function validateLogInForm() 
+ {
+     var valid = true;
+     var username = document.getElementById("uname");
+     var password = document.getElementById("pass");
+
+                 
+     if(!LoginUsername_validation(username)){
+        valid = false;
+     }
+                 
+     if(!LoginPassword_validation(password)){
+         valid = false;
+     }
+         
+
+
+
+ /*checks login username */
+ function LoginUsername_validation(username) 
+ { 
+         if(checkRequired(username)){
+                 document.getElementById('usernameError').style.display = "none";
+                 return true;
+         }
+         else {
+                 document.getElementById('usernameError').style.display = "inline-block";
+                 return false;
+         }
+ }
+ /* checks login password */
+ function LoginPassword_validation(password) 
+ { 
+         if(checkRequired(password)){
+                 document.getElementById('passwordError').style.display = "none";
+                 return true;
+         }
+         else {
+                 document.getElementById('passwordError').style.display = "inline-block";
+                 return false;
+         }
+ }
+}
+
+
+
+ 
+ 
+ 
  function validateForm() 
         {
             var valid = true;
@@ -5,26 +57,69 @@
             var username2 = document.getElementById("sname");
             var password = document.getElementById("pass");
             var date = document.getElementById("dob2");
-
+			var gender = document.getElementById("male");
+			var gender = document.getElementById("female");
+			
+			var a = document.getElementById("pwd1").value;
+			var b = document.getElementById("pwd2").value;
+			
             if(!LoginUsername_validation(username)){
                valid = false;
             }
+			
             if(!LoginPassword_validation(password)){
                 valid = false;
             }
-              if(!SignupUsername_validation(username2)){
+			
+            if(!SignupUsername_validation(username2)){
                valid = false;
             }
+			
+			if(!gender_validation(gender)){
+				valid = false;
+			}
+			
+			
             if(!checkDate(date)) {
                 valid = false;
             }
+			
+			
+			if(!confirmpassword_validation(a)){
+				valid = false;
+			}
+			
+			if(!confirmpassword_validation(b)){
+				valid = false;
+			}
+	
+			return valid;
+		}
+		
+		/*check password*/
+		function confirmpassword_validation(){
+		
+			var password = document.getElementById("pwd1").value;
+			var confirmPassword = document.getElementById("pwd2").value;
+			if (password != confirmPassword) {
+				document.getElementById('pwd2Error').style.display = "inline-block";
+				return false;
+			}
+			document.getElementById('pwd2Error').style.display = "none";
+			return true;
+		}
+		
+		/*select radio button*/
+		function checkGender(){
+			var radio1 = document.getElementById('male').checked;
+			var radio2 = document.getElementById('female').checked;
+			if((radio1=="")&&(radio2=="")){
+				return false;
+			}
 
-            return valid;
-
-
-
-        }
-
+			return true;
+		}
+		
 
         /*checks textbox empty */
         function checkRequired(val) { 
@@ -35,17 +130,29 @@
                 }
                 return true;
         }
-
+		
+		
+		/* checks gender */
+		function gender_validation(gender){
+		        if(checkGender(gender)){
+                        document.getElementById('genderError').style.display = "none";
+                        return true;
+                }
+                else {
+                        document.getElementById('genderError').style.display = "inline-block";
+                        return false;
+                }
+		}
 
         /*checks login username */
         function LoginUsername_validation(username) 
         { 
                 if(checkRequired(username)){
-                        document.getElementById('username2Error').style.display = "none";
+                        document.getElementById('usernameError').style.display = "none";
                         return true;
                 }
                 else {
-                        document.getElementById('username2Error').style.display = "inline-block";
+                        document.getElementById('usernameError').style.display = "inline-block";
                         return false;
                 }
         }
@@ -65,14 +172,16 @@
         function SignupUsername_validation(username2) 
         { 
                 if(checkRequired(username2)){
-                        document.getElementById('usernameError').style.display = "none";
+                        document.getElementById('username2Error').style.display = "none";
                         return true;
                 }
                 else {
-                        document.getElementById('usernameError').style.display = "inline-block";
+                        document.getElementById('username2Error').style.display = "inline-block";
                         return false;
                 }
         }
+		
+
         /*check dob format*/
         function checkDate(inputText) 
         {
@@ -99,4 +208,3 @@
                 document.getElementById('dobError').style.display = "inline-block";
                 return false;
         }
-
